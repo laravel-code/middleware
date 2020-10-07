c65cad254976dd992f4d095b5d774d4c213bae86
raymond <rsvvuuren@hotmail.com>
2020-10-08 00:44:24 +0200

    fix config

--- src/config/oauth.php
+++ src/config/oauth.php
@@ -6,5 +6,5 @@ return [
     'client_id' => env('AUTHORIZATION_API_CLIENT_ID'),
     'client_secret' => env('AUTHORIZATION_API_CLIENT_SECRET'),
     'scopes' => env('AUTHORIZATION_API_SCOPES'),
-    'public_key' => env('AUTHORIZATION_API_HOST', storage_path('oauth-public.key')),
+    'public_key' => env('AUTHORIZATION_PUBLIC_KEY', storage_path('oauth-public.key')),
 ];
